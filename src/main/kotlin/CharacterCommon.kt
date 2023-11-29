2919e1b57a5f6d60e98fbf0badf682b09fe95b45
LENOVO <test23062000@gmail.com>
2023-11-29 13:08:39 +0300

    dzz

--- src/main/kotlin/CharacterCommon.kt
+++ src/main/kotlin/CharacterCommon.kt
@@ -18,17 +18,19 @@ object CharacterCommon {
     }
 
     fun defend(character: Character) {
-        when (character) {
-            is Character.Archer -> {
-                println("${character.name} ${character.ability} использовав способность Ловкость")
-            }
+        with(character) {
+            when (character) {
+                is Character.Archer -> {
+                    println("$name ${character.ability} использовав способность Ловкость")
+                }
 
-            is Character.Mage -> {
-                println("${character.name} ${character.spell} заклинание Темный купол")
-            }
+                is Character.Mage -> {
+                    println("$name ${character.spell} заклинание Темный купол")
+                }
 
-            is Character.Warrior -> {
-                println("${character.name} ${character.ability} применив способность Отражающий света клинка")
+                is Character.Warrior -> {
+                    println("$name ${character.ability} применив способность Отражающий света клинка")
+                }
             }
         }
     }
